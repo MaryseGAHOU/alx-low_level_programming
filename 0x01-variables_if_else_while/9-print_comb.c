9769b2ac542339619bb268b52c3eba95cb4f510f
Maryse GAHOU <marysegahou@gmail.com>
2022-09-09 18:31:20 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Patience, persistence and perspiration
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	n = 48;
+
+	while (n <= 57) /*57 corresponds with ASCII character 9*/
+	{
+		putchar (n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	
+	putchar ('\n');
+	return (0);
+}
