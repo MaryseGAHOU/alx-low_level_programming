e753d7093b905285fe13cfd8a96485172ccb1334
Maryse GAHOU <marysegahou@gmail.com>
2022-09-09 18:53:29 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - The success combination in business
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int a = '0';
+	int b;
+	int c;
+
+	while (a <= '7')
+	{
+		b = a + 1;
+
+		while (b <= '8')
+		{
+			c = b + 1;
+
+			while (c <= '9')
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+
+				if (a == '7' && b == '8' && c == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+				c++;
+
+			}
+			b++;
+
+		}
+		a++;
+
+	}
+
+	return (0);
+
+}
