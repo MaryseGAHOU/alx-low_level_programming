e8ae2a0aa877726797437c7b210edd958c88a0e3
Maryse GAHOU <marysegahou@gmail.com>
2022-09-09 19:01:08 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Software is eating the World
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+
+	int a = 0;
+	int b;
+
+	while (a <= 98)
+	{
+		b = a + 1;
+
+		while (b <= 99)
+		{
+			putchar(a / 10 % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar(b % 10 + '0');
+
+			if (a == 98 && b == 99)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+
+		}
+		a++;
+
+	}
+
+	return (0);
+
+}
