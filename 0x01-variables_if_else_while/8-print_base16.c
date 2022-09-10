0ba37e821a2e322ded06b7eb99549446dd446723
Maryse GAHOU <marysegahou@gmail.com>
2022-09-09 18:24:57 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Hexadecimal
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+
+	int i;
+	char x;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
