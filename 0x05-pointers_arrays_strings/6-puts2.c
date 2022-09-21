480d525b356ee528694d22e64056845ffeb4cd36
Maryse GAHOU <marysegahou@gmail.com>
2022-09-21 07:50:22 -0700

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-
-/**
- * _strlen - returns the length of a string.
- * @s: a pointer to a char
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-	int l, i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	l = i;
-	return (l);
-
-}
 
 /**
  * puts2 - prints one char out of 2 of a string
